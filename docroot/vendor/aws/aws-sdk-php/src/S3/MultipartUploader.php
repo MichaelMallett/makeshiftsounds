fa6b6d1ef87dfbd8e9592a84d5d3eb237eaa10cf
Michael Mallett <mike@generalpie.com>
2018-07-12 16:07:54 +1000

    Drupal and composer updates

--- docroot/vendor/aws/aws-sdk-php/src/S3/MultipartUploader.php
+++ docroot/vendor/aws/aws-sdk-php/src/S3/MultipartUploader.php
@@ -48,6 +48,9 @@ class MultipartUploader extends AbstractUploader
      *   options detailed above to update the commands directly.
      * - part_size: (int, default=int(5242880)) Part size, in bytes, to use when
      *   doing a multipart upload. This must between 5 MB and 5 GB, inclusive.
+     * - prepare_data_source: (callable) Callback to invoke before starting the
+     *   multipart upload workflow. The callback should have a function
+     *   signature like `function () {...}`.
      * - state: (Aws\Multipart\UploadState) An object that represents the state
      *   of the multipart upload and that is used to resume a previous upload.
      *   When this option is provided, the `bucket`, `key`, and `part_size`
@@ -112,16 +115,18 @@ class MultipartUploader extends AbstractUploader
             $source = $this->decorateWithHashes($source, $data);
             $body = Psr7\stream_for();
             Psr7\copy_to_stream($source, $body);
-            $data['ContentLength'] = $body->getSize();
         }
 
+        $contentLength = $body->getSize();
+
         // Do not create a part if the body size is zero.
-        if ($body->getSize() === 0) {
+        if ($contentLength === 0) {
             return false;
         }
 
         $body->seek(0);
         $data['Body'] = $body;
+        $data['ContentLength'] = $contentLength;
 
         return $data;
     }
